f5285d8ffde54f38acdc72c15a4122858c3cf1de
Frank Vinci <fv42wid@yahoo.com>
2016-06-27 00:51:26 +0000

    fixed notifications pulling in already read records

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @notifications = Notification.where(recipient: current_user)
+    @notifications = Notification.where(recipient: current_user, read_at: nil)
   end
 
   def mark_as_read
