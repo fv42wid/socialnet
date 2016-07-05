a78893d40162c8e194cbdb924804b2e35eef009a
Frank Vinci <fv42wid@yahoo.com>
2016-07-05 00:34:58 +0000

    created event scaffold, start video at 3 minutes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160702210717) do
+ActiveRecord::Schema.define(version: 20160705003301) do
+
+  create_table "events", force: :cascade do |t|
+    t.integer  "user_id",        limit: 4
+    t.string   "action",         limit: 255
+    t.integer  "eventable_id",   limit: 4
+    t.string   "eventable_type", limit: 255
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
 
   create_table "friendships", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
