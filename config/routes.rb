7c097bdd77c9f4fe5fc10c70640af0ae4eadcb6e
Frank Vinci <fv42wid@yahoo.com>
2016-07-08 21:39:20 +0000

    updated routes to change root to events

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
     end
   end
   
-  root :to => redirect('/posts')
+  root :to => redirect('/events')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
