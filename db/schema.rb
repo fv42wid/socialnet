154d6a0e77aeb17d486cdce6896cb7f7eec9056a
Frank Vinci <fv42wid@yahoo.com>
2016-07-16 15:33:45 +0000

    added scaffold for album, now nest under user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713183321) do
+ActiveRecord::Schema.define(version: 20160716153225) do
+
+  create_table "albums", force: :cascade do |t|
+    t.integer  "user_id",     limit: 4
+    t.string   "title",       limit: 255
+    t.string   "description", limit: 255
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.integer  "user_id",        limit: 4
