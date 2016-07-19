330eae90b27ea0a71a38bce34c373d87ad717d63
Frank Vinci <fv42wid@yahoo.com>
2016-07-19 00:27:31 +0000

    pulling in first album picture as thumnail, now link to show album and clean that up

--- app/helpers/albums_helper.rb
+++ app/helpers/albums_helper.rb
@@ -1,2 +1,10 @@
 module AlbumsHelper
+
+  def album_thumbnail(album)
+    if album.pictures.count > 0
+      image_tag album.pictures.first.location, :class => 'img-responsive'
+    else
+      image_tag 'http://placehold.it/150x100', :class => 'img-responsive'
+    end
+  end
 end
