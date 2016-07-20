572050c96871b2da4e2bcd1b9dadff2c4bd7c1e3
Frank Vinci <fv42wid@yahoo.com>
2016-07-20 18:10:01 +0000

    changed post create redirect to events path

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,7 +31,7 @@ class PostsController < ApplicationController
     respond_to do |format|
       if @post.save
         current_user.events.create(action: "posted", eventable: @post)
-        format.html { redirect_to posts_path, notice: 'Post was successfully created.' }
+        format.html { redirect_to events_path, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { redirect_to posts_path, flash: {errors: @post.errors} }
