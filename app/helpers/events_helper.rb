7448b5821cc388f0899a9f2514cce5c2540b4357
Frank Vinci <fv42wid@yahoo.com>
2016-07-21 23:09:14 +0000

    updated _posted_post partial to make mentions link to users

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,9 @@
 module EventsHelper
+
+  def linked_users(body)
+    body.gsub /@([\w]+\@[\w]+\.[\w]+)/ do |match|
+      link_to $1, user_path(User.select('id').where(email: $1).ids[0])
+    end.html_safe
+  end
+
 end
