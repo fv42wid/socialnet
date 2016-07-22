9b751a4784c472cdbb45316cea911bd991238c42
Frank Vinci <fv42wid@yahoo.com>
2016-07-22 22:08:46 +0000

    added caption field to picture form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160720133246) do
+ActiveRecord::Schema.define(version: 20160722220122) do
 
   create_table "albums", force: :cascade do |t|
     t.integer  "user_id",     limit: 4
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20160720133246) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
     t.integer  "album_id",   limit: 4
+    t.string   "caption",    limit: 255
   end
 
   create_table "posts", force: :cascade do |t|
