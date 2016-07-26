351406ceecee1fbd64ff63662cbd03d7501d08f8
Frank Vinci <fv42wid@yahoo.com>
2016-07-26 14:02:58 +0000

    remote form works and alert displays, now render the html comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -29,6 +29,7 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @comment.save
         format.html { redirect_to events_path, notice: 'Comment was successfully created.' }
+        format.js {}
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
